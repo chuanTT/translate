ff680a589bc7e9f01bdea5aeee8899128e3ed37d
chuantt <chuan123789@gmail.com>
2024-07-22 17:43:57 +0700

    update

--- language.js
+++ language.js
@@ -1,14 +1,27 @@
 import { JSDOM } from "jsdom";
-import { appendFileConfig, funcConvertValue, readFileToPathConfig, writeFileConfig } from "./function.js";
-const html = readFileToPathConfig("language.html")
+import {
+  appendFileConfig,
+  funcConvertValue,
+  readFileToPathConfig,
+  writeFileConfig,
+} from "./function.js";
+const html = readFileToPathConfig("language.html", false);
 const dom = new JSDOM(html);
 
-writeFileConfig("language-support.txt", "")
+writeFileConfig("language-support.txt", "");
 
+const objLang = {};
 dom.window.document
   .querySelectorAll("a:not([href='#'])")
   .forEach(async (elem) => {
     const text = elem.textContent.trim();
     const value = elem.getAttribute("href")?.slice(1);
-    appendFileConfig("language-support.txt", `${funcConvertValue(value)} - ${text}\r\n`)
+    const key = funcConvertValue(value)
+    objLang[key] = text;
+    appendFileConfig(
+      "language-support.txt",
+      `${funcConvertValue(value)} - ${text}\r\n`
+    );
   });
+
+writeFileConfig("lang.json", objLang);
